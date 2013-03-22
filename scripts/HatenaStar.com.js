8837f90bdc3b418843a534f2f61edcf2442951a4
Wakaba <wakabatan@hatena.ne.jp>
2013-03-22 17:12:15 +0900

    regen

--- scripts/HatenaStar.com.js
+++ scripts/HatenaStar.com.js
@@ -4487,6 +4487,8 @@ Hatena.Star.EntryLoader = new Ten.Class({
             result = window.location;
         } else if (selector == 'parent') {
             result = parent;
+        } else if (selector.match(/^link\[rel~?="?canonical"?\]$/)) {
+            result = Ten.querySelector(selector);
         } else {
             result = Ten.Selector.getElementsBySelector(selector,parent)[0];
         }
